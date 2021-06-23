e13023362dd1dbe24962c5e264d61779493a3227
venustuspl <venustus.pl@gmail.com>
2021-06-23 16:10:18 +0200

    feat: added exercise for React this.setState

--- react/5-PrzyciskDodajTekst-cz1/app.js
+++ react/5-PrzyciskDodajTekst-cz1/app.js
@@ -6,19 +6,38 @@ class App extends React.Component {
     number: 2
   }
 
-  handleClick = () => {
+  handleClickA = () => {
     // this.state.text += "a";
     // console.log(this.state.text);
 
-    const letter = "a"
+    const letterA = "a"
     this.setState({
-      text: this.state.text + letter
+      text: this.state.text + letterA
     })
+  }
+
+  handleClickB = () => {
+    const letterB = "b"
+    this.setState(
+      {
+        text: this.state.text + letterB
+      }
+    )
+  }
+
+  handleClickClear = () => {
+    this.setState(
+      {
+        text : ""
+      }
+    )
   }
   render() {
     return (
       <React.Fragment>
-        <button onClick={this.handleClick}>Dodaj "A"</button>
+        <button onClick={this.handleClickA}>Dodaj "A"</button>
+        <button onClick={this.handleClickB}>Dodaj "B"</button>
+        <button onClick={this.handleClickClear}> Wyczyść </button>
         <h1>{this.state.text}</h1>
       </React.Fragment>
     )
