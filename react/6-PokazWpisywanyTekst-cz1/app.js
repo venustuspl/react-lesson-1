2d5c1bcaf7e875c47fcad7509a422940ce610d8f
venustuspl <venustus.pl@gmail.com>
2021-06-26 07:41:21 +0200

    feat: added lesson for inputbox

--- react/6-PokazWpisywanyTekst-cz1/app.js
+++ react/6-PokazWpisywanyTekst-cz1/app.js
@@ -27,4 +27,38 @@ class App extends React.Component {
   }
 }
 
+class MyApp extends React.Component {
+
+  state = {
+    value: ""
+  }
+
+  handleInputChange = (e) => {
+    this.setState(
+      {
+        value: e.target.value
+      }
+    )
+  }
+
+  handleResetClick = () => {
+    this.setState(
+      {
+        value: ""
+      }
+    )
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <input type="text" value={this.state.value} placeholder="Wpisz coś" onChange={this.handleInputChange}  />
+        <button onClick={this.handleResetClick}>Reset</button>
+        <h4 className="title">{this.state.value.toLowerCase()}</h4>
+      </React.Fragment>
+    )     
+  }
+}
+
 ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<MyApp />, document.getElementById('myroot'))
