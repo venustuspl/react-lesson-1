eacbb968617196a144851a5855233b603f8736c8
venustuspl <venustus.pl@gmail.com>
2021-08-11 21:05:15 +0200

    feature: added discount logic

--- react/v2/14-17-Order-Restaurant/Header.js
+++ react/v2/14-17-Order-Restaurant/Header.js
@@ -1,11 +1,14 @@
 const Header = (props) => {
   const activeItems = props.items.filter(item => item.active)
   const number = activeItems.length
+  const price = number * 3;
   return (
     <header>
       <h2>Wielkość zamówienia: {number}</h2>
-      <h2>Cena: 3zł </h2>
-      <h2>Do zapłaty: {number ? `${number * 3} złotych` : 'nie kupujesz, to nie płacisz'}</h2>
+      <h2>Cena 1szt.: 3zł </h2>
+      <h2>Rabat: 10% </h2>
+      <h2>Do zapłaty: {number ? `${price} złotych` : 'nie kupujesz, to nie płacisz'}</h2>
+      <h2>{number ? `Kwota po rabacie ${ price - (price * 0.1) } złotych` : null }</h2>
     </header>
   )
 } 
